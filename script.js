cbfe5294ab63295a037692d39ea28b923c667e37
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-02 09:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+function getRandomDelay() {
+  return Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds in milliseconds
+}
+
+function createPromise(delay) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(delay / 1000); // Resolving with time taken in seconds
+    }, delay);
+  });
+}
+
+const promises = [
+  createPromise(getRandomDelay()),
+  createPromise(getRandomDelay()),
+  createPromise(getRandomDelay()),
+  createPromise(getRandomDelay()),
+  createPromise(getRandomDelay())
+];
+
+Promise.any(promises)
+.then(result => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = `The first resolved promise result is: ${result.toFixed(3)}`;
+})
+.catch(error => {
+  console.log(error);
+});
